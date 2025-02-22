0579a7cf69e73b4eca71b51f07d4abc0a706d11c
garvjainus <garv.jainn@gmail.com>
2025-02-22 01:00:30 -0500

    Initial Commit

---
+++ courtside/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  courtside
+//
+//  Created by Garv Jain on 2/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ courtside/courtsideApp.swift
@@ -0,0 +1,17 @@
+//
+//  courtsideApp.swift
+//  courtside
+//
+//  Created by Garv Jain on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct courtsideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
